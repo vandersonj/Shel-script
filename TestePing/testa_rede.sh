db66d440ec591e5dd960bb0627336caf4d910f27
vanderson.jahn <vanderson.jahn@clamed.com.br>
2022-09-09 14:06:43 -0300

    Add Teste Ping

---
+++ TestePing/testa_rede.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+#   Lista as Redes respondendo na lista de Rede, onde o temos um padrão de endereçamento IP.
+#
+#
+#--------------------------------------------------------------
+
+HOST=11
+for REDE in $(cat listarede.txt); 
+do
+    IP=$REDE.$HOST
+    # Da um ping no endereco ip verificando se houve recebimento do pacote
+    # contando a linha de retorno.
+    OK=$(ping -c1 $IP | fgrep "1 received" | wc -l)
+    
+    # Testa se a linha de retorno e igual a 1 e exibe a mensage
+    if [ $OK == 1 ]
+    then
+        echo $IP
+        #echo $IP >> ativos.txt
+    fi
+done 
